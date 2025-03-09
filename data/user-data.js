5f5deed30b68ea5581e1c9c3d214459cb19d86cd
VincentTuannn <vincenttuan1098@gmail.com>
2025-03-09 12:28:24 +0700

    add hehehe

--- data/user-data.js
+++ data/user-data.js
@@ -1,20 +1,10 @@
 export const userData = {
-  githubUser: "nguyenminhphuc15",
-  devUsername: "nguyenminhphuc15",
-  github: "https://github.com/nguyenminhphuc15",
-  facebook: "https://www.facebook.com/nguyen.phuc.191857/",
-  resume:
-    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
-  skills: [
-    "React",
-    "NextJS",
-    "Redux",
-    "Express",
-    "MySql",
-    "MongoDB",
-    "Postgres",
-    "Docker",
-    "AWS",
-  ],
-  timezone: "+6",
+  githubUser: 'VincentTuannn',
+  devUsername: "Nguyen Quoc Tuan",
+  github: 'https://github.com/VincentTuannn',
+  facebook: 'https://www.facebook.com/nguyen.quoc.tuan.220704/',
+  linkedIn: 'https://www.linkedin.com/in/qu%E1%BB%91c-tu%E1%BA%A5n-nguy%E1%BB%85n-b3069a332/',
+  stackOverflow: 'https://stackoverflow.com/users/24136265/qu%e1%bb%91c-tu%e1%ba%a5n-nguy%e1%bb%85n',
+  skills: ['React',  'Angular', 'MySql', '.NET', 'MongoDB', 'Figma', 'Docker', 'AWS'],
+  timezone: '+7'
 };
